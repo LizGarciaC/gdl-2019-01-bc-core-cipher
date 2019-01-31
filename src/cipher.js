d8046e5c8c21b6e3c89a31b28a6b9fac97d39953
LizGarciaC <lunanobel@hotmail.com>
2019-01-31 12:46:48 -0600

    Actualización nombre variables de acuerdo al archivo Readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,23 @@
 window.cipher = {
+  let x;
+  let n=0;
+  let i=0;
+  let codeMensaje;
+
+
+let code=()=>{
+  x=document.getElementById('boxSting').value;
+  n=document.getElementById('boxOffset').value;
+
+  for (i=0; i<=x.lengt, i++);
+  codeMensaje= ((x.charCodeAt(i))-65+n)%26+65
+  prompt (codeMensaje);
+
+}
+
+
+
+
+
   // ... 
 };
