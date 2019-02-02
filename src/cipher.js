0c31edc612613033121dd0774522d082b9700d8a
LizGarciaC <lunanobel@hotmail.com>
2019-02-01 21:10:04 -0600

    Se agrega la función Encode, que aún no está terminad. Se modifica también estructura HTML

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,10 @@
 window.cipher = {
-  let x;
-  let n=0;
-  let i=0;
-  let codeMensaje;
+  //cipher.encode(offset,message);
+  encode:(offset, message)=> {
+    return message;
 
+  }
 
-let code=()=>{
-  x=document.getElementById('boxSting').value;
-  n=document.getElementById('boxOffset').value;
-
-  for (i=0; i<=x.lengt, i++);
-  codeMensaje= ((x.charCodeAt(i))-65+n)%26+65
-  prompt (codeMensaje);
-
-}
 
 
 
---
+++ src/index.js
@@ -0,0 +1,17 @@
+ 
+
+let encode=()=> {
+    let offset;  // variable que guarda el valor contenido en la caja de texto desplazamientos
+    offset=document.getElementById("boxOffset").value;  // Asigna el valor de la caja (crea la interfaz) a la variable
+    console.log(offset); //muestra en la consola el valor asignado
+    let message; // variable que guarda el valor contenido en la caja de texto Mensaje a Codificar
+    message=document.getElementById("boxString").value; // // Asigna el valor de la caja (crea la interfaz) a la variable
+    console.log(message); ////muestra en la consola el valor asignado
+
+    let messageCode;
+    messageCode=cipher.encode(offset,message);
+
+    let boxMessageCoded=document.getElementById("boxCoded"); // Crea la variable para enviar el valor a la caja de texto en html
+    boxMessageCoded.value=messageCode; //Asigna el valor a la caja de texto
+    
+}
