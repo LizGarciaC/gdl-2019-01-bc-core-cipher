32add83bc81c5d44b226480d9f384b34407e56ee
LizGarciaC <lunanobel@hotmail.com>
2019-02-02 19:45:04 -0600

    Se agrega la función cifrado, para números positivos y negativos en los desplazamientos

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,22 @@
 window.cipher = {
   //cipher.encode(offset,message);
   encode:(offset, message)=> {
-    return message;
+    let cMessage=[];
 
+    for (let count=0; count<message.length; count++){ //Ciclo para acceder a cada letra individual del mensaje
+      let charAscii=(message.charCodeAt(count)-65+offset)%26+65; // Toma el valor de cada letra, lo convierte a Ascii, y lo codifica segun la ctd de desplazamientos solicitados (offset)
+      if (charAscii<65){ // Control de flujo que se ejecuta en caso de que el desplazamiento solicitado sea negativo
+      charAscii=charAscii+26; // nos regresa el valor a la primer posición  de la tabla
+      }
+        
+      cMessage.push(String.fromCharCode(charAscii));
+    }
+
+    return cMessage.join("");
+  },
+
+  decode:(offset, message)=>{
+    return;
   }
-
-
-
-
-
-
   // ... 
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
  
 
-let encode=()=> {
+let encode = ()=> {
     let offset;  // variable que guarda el valor contenido en la caja de texto desplazamientos
     offset=document.getElementById("boxOffset").value;  // Asigna el valor de la caja (crea la interfaz) a la variable
     console.log(offset); //muestra en la consola el valor asignado
     let message; // variable que guarda el valor contenido en la caja de texto Mensaje a Codificar
-    message=document.getElementById("boxString").value; // // Asigna el valor de la caja (crea la interfaz) a la variable
+    message=document.getElementById("boxString").value.toUpperCase(); // // Asigna el valor de la caja (crea la interfaz) a la variable
     console.log(message); ////muestra en la consola el valor asignado
+    
 
     let messageCode;
-    messageCode=cipher.encode(offset,message);
-
+    messageCode=cipher.encode(Number(offset),message);
+    console.log(messageCode);
     let boxMessageCoded=document.getElementById("boxCoded"); // Crea la variable para enviar el valor a la caja de texto en html
     boxMessageCoded.value=messageCode; //Asigna el valor a la caja de texto
     
-}
+};
