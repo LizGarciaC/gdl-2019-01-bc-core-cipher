e0253c7c6085d0f0cc6c589fd6ae3d8816e92ba1
LizGarciaC <lunanobel@hotmail.com>
2019-02-04 22:11:40 -0600

    Se agrega la función de descifrar y boton limpiar, ya funcionan

--- src/cipher.js
+++ src/cipher.js
@@ -5,18 +5,34 @@ window.cipher = {
 
     for (let count=0; count<message.length; count++){ //Ciclo para acceder a cada letra individual del mensaje
       let charAscii=(message.charCodeAt(count)-65+offset)%26+65; // Toma el valor de cada letra, lo convierte a Ascii, y lo codifica segun la ctd de desplazamientos solicitados (offset)
+     
       if (charAscii<65){ // Control de flujo que se ejecuta en caso de que el desplazamiento solicitado sea negativo
-      charAscii=charAscii+26; // nos regresa el valor a la primer posición  de la tabla
+      charAscii=charAscii+26; // Coloca el valor del caracter en ascii, dentro de la tabla de letras
       }
         
-      cMessage.push(String.fromCharCode(charAscii));
+      cMessage.push(String.fromCharCode(charAscii)); //
     }
 
     return cMessage.join("");
   },
 
+
   decode:(offset, message)=>{
-    return;
+    offset=offset*(-1);
+    let dMessage=[];
+
+    for (let count=0; count<message.length; count++){ //Ciclo para acceder a cada letra individual del mensaje
+      let charAscii=(message.charCodeAt(count)-65+offset)%26+65; // Toma el valor de cada letra, lo convierte a Ascii, y lo codifica segun la ctd de desplazamientos solicitados (offset)
+     
+      if (charAscii<65){ // Control de flujo que se ejecuta en caso de que el desplazamiento solicitado sea negativo
+      charAscii=charAscii+26; // Coloca el valor del caracter en ascii, dentro de la tabla de letras
+      }
+        
+      dMessage.push(String.fromCharCode(charAscii)); //
+    }
+
+    return dMessage.join("");
+
   }
   // ... 
 };
