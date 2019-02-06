f749bacb9da2a355b20a1405a2aa592a66c6ab9c
LizGarciaC <lunanobel@hotmail.com>
2019-02-06 10:53:57 -0600

    Se agrega reconocimiento de espacios vacios

--- src/cipher.js
+++ src/cipher.js
@@ -4,13 +4,20 @@ window.cipher = {
     let cMessage=[];
 
     for (let count=0; count<message.length; count++){ //Ciclo para acceder a cada letra individual del mensaje
+  
       let charAscii=(message.charCodeAt(count)-65+offset)%26+65; // Toma el valor de cada letra, lo convierte a Ascii, y lo codifica segun la ctd de desplazamientos solicitados (offset)
-     
+
+      
       if (charAscii<65){ // Control de flujo que se ejecuta en caso de que el desplazamiento solicitado sea negativo
+       
+        if (charAscii=-32){
+          charAscii=6;
+        }
+        
       charAscii=charAscii+26; // Coloca el valor del caracter en ascii, dentro de la tabla de letras
       }
         
-      cMessage.push(String.fromCharCode(charAscii)); //
+      cMessage.push(String.fromCharCode(charAscii)); 
     }
 
     return cMessage.join("");
@@ -25,6 +32,11 @@ window.cipher = {
       let charAscii=(message.charCodeAt(count)-65+offset)%26+65; // Toma el valor de cada letra, lo convierte a Ascii, y lo codifica segun la ctd de desplazamientos solicitados (offset)
      
       if (charAscii<65){ // Control de flujo que se ejecuta en caso de que el desplazamiento solicitado sea negativo
+        
+        if (charAscii=-32){
+          charAscii=6;
+        }
+  
       charAscii=charAscii+26; // Coloca el valor del caracter en ascii, dentro de la tabla de letras
       }
         
--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
  
+let btn1 = document.getElementById("btn1");
+  btn1.addEventListener ('click', ()=>{
 
-let encode = ()=> {
     let offset;  // variable que guarda el valor contenido en la caja de texto desplazamientos
     offset=document.getElementById("boxOffset").value;  // Asigna el valor de la caja (crea la interfaz) a la variable
-    console.log(offset); //muestra en la consola el valor asignado
+    //console.log(offset); //muestra en la consola el valor asignado
     let message; // variable que guarda el valor contenido en la caja de texto Mensaje a Codificar
     message=document.getElementById("boxString").value.toUpperCase(); // // Asigna el valor de la caja (crea la interfaz) a la variable
     console.log(message); ////muestra en la consola el valor asignado
     
 
-    let messageCode;
+    let messageCode; 
     messageCode=cipher.encode(Number(offset),message);
     console.log(messageCode);
     let boxMessageCoded=document.getElementById("boxCoded"); // Crea la variable para enviar el valor a la caja de texto en html
     boxMessageCoded.value=messageCode; //Asigna el valor a la caja de texto
     
-};
+});
 
-let decode = ()=> {
+  let btn2 = document.getElementById("btn2");
+  btn2.addEventListener ('click', ()=>{
+
+
     let offset;  // variable que guarda el valor contenido en la caja de texto desplazamientos
     offset=document.getElementById("boxOffset").value;  // Asigna el valor de la caja (crea la interfaz) a la variable
     console.log(offset); //muestra en la consola el valor asignado
@@ -32,9 +36,12 @@ let decode = ()=> {
     let boxMessageCoded=document.getElementById("boxCoded"); // Crea la variable para enviar el valor a la caja de texto en html
     boxMessageCoded.value=messageCode; //Asigna el valor a la caja de texto
 
-};
+});
 
-let clean = ()=>{
+//let clean = ()=>{
+  let btn3=document.getElementById('btn3');
+  btn3.addEventListener("click", ()=>{
+
   let offset=document.getElementById("boxOffset"); 
   offset.value="";
   let message=document.getElementById("boxString");
@@ -45,5 +52,5 @@ let clean = ()=>{
 
 
 
-};
+});
 
